fd1efe390aef2fecca4004be5ac8d5b9b0ce5aa6
hoadq <hoadinhquoc@gmail.com>
2018-12-16 20:09:26 +0700

    *Can roll

--- Assets/Scripts/RollingNumber.cs
+++ Assets/Scripts/RollingNumber.cs
@@ -10,6 +10,7 @@ public class RollingNumber : MonoBehaviour {
 	[SerializeField]TextMeshProUGUI textNumber;
 
 	[SerializeField]float CHANGING_TIME = 0.5f;
+	[SerializeField]int START_NUMBER = 0;
 	[SerializeField]int TOTAL_NUMBER = 10;
 
 	[SerializeField]List<Sprite> numberSprites;
@@ -17,22 +18,48 @@ public class RollingNumber : MonoBehaviour {
 	
 	float _timer = 0f;
 	int _number = 0;
-	void Start () {
+	bool _isRolling = false;
+	int _targetNumber = -1;
+	void Awake () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+		if(!_isRolling) return;
+
 		_timer += Time.deltaTime;
 
 		if(_timer >= CHANGING_TIME)
 		{
-			_number = (_number + 1) % TOTAL_NUMBER;
+			_number = (_number + 1) % TOTAL_NUMBER + START_NUMBER;
 			textNumber.text = _number.ToString();
 			_timer = 0f;
 
 			numberImage.sprite = numberSprites[_number];
+
+			if(_targetNumber != -1)
+			{
+				if(_number == _targetNumber)
+				{
+					_isRolling = false;
+					_targetNumber = -1;
+				}
+			}
 		}
+	}
+
+	public void StartRolling()
+	{
+		_timer = 0f;
+		_number = 0;
+
+		_isRolling = true;
+	}
+
+	public void StopRollingAt(int targetNumber)
+	{
+		_targetNumber = targetNumber;
 	}
 }
--- Assets/Scripts/SlotMachine.cs
+++ Assets/Scripts/SlotMachine.cs
@@ -3,14 +3,79 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SlotMachine : MonoBehaviour {
-
+	[SerializeField] int minNumber = 200;
+	[SerializeField] int maxNumber = 360;
+	[SerializeField] float stopTime = 4f;
+	[SerializeField] List<RollingNumber> rollingSlots;
 	// Use this for initialization
-	void Start () {
-		
-	}
 	
+	List<int> _numberPool = new List<int>();
+	int _winningNumber = -1;
+	List<int> _winningNumbers = new List<int>();
+	float _timer = 0f;
+	bool _isRolling = false;
+	int _currentSlotIndex = 0;
+	void Awake()
+	{
+		Reset();
+	}
 	// Update is called once per frame
 	void Update () {
+		if(!_isRolling) return;
+
+		float dt = Time.deltaTime;
+		_timer += dt;
+
+		if(_timer > stopTime)
+		{
+			_timer = 0f;
+
+			rollingSlots[_currentSlotIndex].StopRollingAt(_winningNumbers[_currentSlotIndex]);
+
+			_currentSlotIndex++;
+
+			if(_currentSlotIndex >= rollingSlots.Count)
+			{
+				_isRolling = false;
+			}
+		}
+
+	}
+
+	public void OnRollingPressed()
+	{
+		rollingSlots.ForEach(x=>x.StartRolling());
+
+		int index = Random.Range(0, _numberPool.Count);
+		_winningNumber =  _numberPool[index];
+		_numberPool.RemoveAt(index);
 		
+		_winningNumbers.Clear();
+		int tempWinningNumber = _winningNumber;
+		
+		for(int i = 0 ; i < rollingSlots.Count; i++)
+		{
+			int addNum = tempWinningNumber % 10;
+			_winningNumbers.Insert(0, addNum);
+			tempWinningNumber /= 10;
+		}
+
+		_timer = 0f;
+		_currentSlotIndex = 0;
+		_isRolling = true;
+	}
+
+	public void Reset()
+	{
+		_numberPool.Clear();
+
+		for(int i = minNumber; i <= maxNumber; i++)
+		{
+			_numberPool.Add(i);
+		}
+		_isRolling = false;
+		_timer = 0f;
+
+		_currentSlotIndex = 0;
 	}
 }
